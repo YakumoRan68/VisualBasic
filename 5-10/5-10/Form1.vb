857da458401fb6d0dda4fd8104762500c61849f6
YakumoRan <37825675+YakumoRan68@users.noreply.github.com>
2018-04-10 15:52:31 +0900

    4/10 ㅋㅁ

---
+++ 5-10/5-10/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim start, final As Integer
+        start = TextBox1.Text
+        final = TextBox2.Text
+
+        For i = start To final Step 1
+            For j = 1 To 9 Step 1
+                TextBox3.AppendText(i & " * " & j & " = " & i * j & vbCrLf)
+            Next
+            TextBox3.AppendText(vbCrLf & vbCrLf)
+        Next
+    End Sub
+End Class
