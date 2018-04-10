200431762e6d1afde424ab254ab88eb26ad6d557
YakumoRan <37825675+YakumoRan68@users.noreply.github.com>
2018-04-10 14:55:47 +0900

    ㅋㅁ

--- 5-8/5-8/Form1.vb
+++ 5-8/5-8/Form1.vb
@@ -2,8 +2,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim sum, i As Integer
         sum = 0
-        For i = 1 To 10 Step 1
-            sum = sum + 5
+        For i = 0 To 20 Step 5
+            sum = sum + i
         Next i
         TextBox1.Text = sum
     End Sub
