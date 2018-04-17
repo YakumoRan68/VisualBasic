83331c70539f31b392a6f45db34838c9623b9cca
YakumoRan <37825675+YakumoRan68@users.noreply.github.com>
2018-04-17 13:12:47 +0900

    과제수정

--- 연습문제5장/5-7/5-7/Form1.vb
+++ 연습문제5장/5-7/5-7/Form1.vb
@@ -1,5 +1,9 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text > TextBox2.Text Then
+            End
+        End If
+
         Dim a, b, i, sum As Integer
 
         a = TextBox1.Text
